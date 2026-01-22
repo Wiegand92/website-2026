4dfdb455ed83994fb6fd44fb3e6d7cc1310b7161
Alex Wiegand <wiegand.alex@gmail.com>
2026-01-21 20:40:12 -0500

    fix base path in photolinks

--- src/lib/components/website-components/PhotoLink.svelte
+++ src/lib/components/website-components/PhotoLink.svelte
@@ -3,27 +3,28 @@
   import books from '$lib/assets/images/icons/books.jpeg';
   import mission from "$lib/assets/images/icons/mission.jpeg";
   import resources from "$lib/assets/images/icons/resources.jpeg";
+  import { base } from '$app/paths';
 
   const cardStyles = "p-0 aspect-square w-48 md:w-64 lg:w-72 lg:mx-10 hover:scale-105 transition-transform duration-300 ease-in-out justify-center";
 </script>
 <div class="flex lg:flex-row lg:justify-center w-full my-10 flex-col items-center gap-4">
     <Card.Root class={cardStyles}>
         <Card.Content>
-            <a href="/about/books">
+            <a href="{base}/about/books">
                 <img src={books} alt="Books available through bbtb"/>
             </a>
         </Card.Content>
     </Card.Root>
     <Card.Root class={cardStyles}>
         <Card.Content>
-            <a href="/about/mission">
+            <a href="{base}/about/mission">
                 <img src={mission} alt="Our Mission"/>
             </a>
         </Card.Content>
     </Card.Root>
     <Card.Root class={cardStyles}>
         <Card.Content>
-            <a href="/resources/how-to">
+            <a href="{base}/resources/how-to">
                 <img src={resources} alt="Resources"/>
             </a>
         </Card.Content>
