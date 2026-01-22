895125c749d5829ef306286bd985c522529e47ac
Alex Wiegand <wiegand.alex@gmail.com>
2026-01-22 18:36:42 -0500

    more styling fixes

--- src/lib/components/website-components/NavBar.svelte
+++ src/lib/components/website-components/NavBar.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <div class="flex flex-col w-full items-center justify-center p-0 shadow-md bg-(--bbtb-secondary-background)">
-    <img src={websiteLogo} class="p-8" alt="Website Logo" width="200px" />
+    <img src={websiteLogo} class="p-8 w-48 lg:w-(200px)" alt="Website Logo" />
     <span class="mb-4 w-full flex flex-col items-center justify-center">
-        <h1 class="text-center text-6xl text-(--bbtb-foreground)">Building Bonds Through Books</h1>
-        <sub class="text-center text-lg italic p-1 text-(--bbtb-foreground)">Bonding Beyond Sentencing: Uniting Families Through Books</sub>
+        <h1 class="text-center text-2xl lg:text-6xl text-(--bbtb-foreground)">Building Bonds Through Books</h1>
+        <sub class="text-center text-md italic p-1 text-(--bbtb-foreground)">Bonding Beyond Sentencing: Uniting Families Through Books</sub>
     </span>
     <NavMenu />
 </div>
--- src/lib/components/website-components/PhotoLink.svelte
+++ src/lib/components/website-components/PhotoLink.svelte
@@ -5,35 +5,29 @@
   import resources from "$lib/assets/images/icons/resources.jpeg";
   import { resolve } from '$app/paths';
 
-  const cardStyles = "p-0 aspect-square lg:mx-10 hover:scale-105 transition-transform duration-300 ease-in-out justify-center";
-  const imgStyles = "h-auto w-48 md:w-64 lg:w-72";
+  const cardStyles = "p-0 lg:mx-10 hover:scale-105 transition-transform duration-300 ease-in-out justify-center";
+  const imgStyles = "h-auto w-48 md:w-64 lg:w-72 rounded-lg";
 </script>
 <div class="flex lg:flex-row lg:justify-center w-full my-10 flex-col items-center gap-4">
     <Card.Root class={cardStyles}>
-        <Card.Content>
+        <Card.Content class="p-1">
             <a href={resolve("/about/books")}>
                 <img src={books} class={imgStyles} alt="Books available through bbtb"/>
             </a>
         </Card.Content>
     </Card.Root>
     <Card.Root class={cardStyles}>
-        <Card.Content>
+        <Card.Content class="p-1">
             <a href={resolve("/about/mission")}>
                 <img src={mission} class={imgStyles} alt="Our Mission"/>
             </a>
         </Card.Content>
     </Card.Root>
     <Card.Root class={cardStyles}>
-        <Card.Content>
+        <Card.Content class="p-1">
             <a href={resolve("/resources/how-to")}>
                 <img src={resources} class={imgStyles} alt="Resources"/>
             </a>
         </Card.Content>
     </Card.Root>
 </div>
-<style lang='postcss'>
-    img{
-        height: fit-content;
-        padding: 0rem;
-    }
-</style>
